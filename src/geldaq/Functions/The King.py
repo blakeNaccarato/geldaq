9bc8f017595df4c96379c03f6680bc5195c3ad5c
asennain <asennaini@gmail.com>
2022-12-02 23:31:59 -0800

    Created glorious function

---
+++ src/geldaq/Functions/The King.py
@@ -0,0 +1,14 @@
+"""
+The purpose of this function is to impart the truth
+and nothing but the truth
+"""
+
+
+def king(size):
+    if size < 12:
+        print("Heresy!")
+    else:
+        print("His Majesty Abdul Sennain has a ", size, " inch schlong.")
+
+
+king(12)
