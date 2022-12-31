91af7194b9336d2ab4c5142d65ff574e7dd735ba
asennain <asennaini@gmail.com>
2022-12-31 00:17:42 -0800

    Added docstring information - ignore previous commit

--- src/geldaq/Functions/TheKing.py
+++ src/geldaq/Functions/TheKing.py
@@ -1,13 +1,16 @@
-"""
-The purpose of this function is to impart the truth
-and only the truth
-"""
-
 # Predefined parameter "size", if it is not defined
 # by the user it will default to this value
 
 
 def king(size=12):
+
+    # Doc-strings are used at the beginning of
+    # modules, functions, classes, etc. and provides details about what the code does
+
+    """
+    The purpose of this function is to impart the truth
+    and only the truth
+    """
     if size < 12:
         print("Heresy!")
     else:
@@ -15,3 +18,12 @@ def king(size=12):
 
 
 king()
+# ^ Because there is a docstring associated with the king() function, it will appear
+#       when you hover over it. This is convenient when you quickly
+#       need to know what the function does, information about its parameters, etc.
+
+print(king.__doc__)
+# ^ You can actually print the docstring using func.__doc__ (two underscores).
+# Perhaps this may be useful if the information is really long.
+# This will be very useful when creating graphing functions with multiple parameters
+#   to keep track of.
